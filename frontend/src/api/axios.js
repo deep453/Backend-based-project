debb83289f6891ceee118086c44b2d84dde7c3e7
Deep <kumardeepranjan66@gmail.com>
2026-07-31 11:44:24 +0530

    vite

--- frontend/src/api/axios.js
+++ frontend/src/api/axios.js
@@ -2,7 +2,7 @@ import axios from "axios";
 
 const api = axios.create({
     baseURL: process.env.NODE_ENV === "production" 
-        ? "https://onrender.com" 
+        ? "https://onrender.com" // Make sure this matches your exact backend link
         : "http://localhost:8000/api/v1",
     withCredentials: true,
     headers: {
