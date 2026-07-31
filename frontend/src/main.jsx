bc514d53bf966d296430addf51248a4831c77a81
Deep <kumardeepranjan66@gmail.com>
2026-07-31 12:17:11 +0530

    main fix

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -8,24 +8,24 @@ import "./index.css";
 import { AuthProvider } from "./context/AuthContext";
 import { Toaster } from "react-hot-toast";
 
-
 ReactDOM.createRoot(document.getElementById("root")).render(
     <React.StrictMode>
-        <BrowserRouter>
+        {/* Added basename attribute so React Router recognizes your GitHub repository folder subpath */}
+        <BrowserRouter basename="/Skill_Nova">
             <AuthProvider>
                 <App />
                 <Toaster
-        position="top-right"
-        reverseOrder={false}
-        toastOptions={{
-            duration: 3000,
-            style: {
-                background: "#111827",
-                color: "#fff",
-                border: "1px solid #ef4444",
-            },
-        }}
-    />
+                    position="top-right"
+                    reverseOrder={false}
+                    toastOptions={{
+                        duration: 3000,
+                        style: {
+                            background: "#111827",
+                            color: "#fff",
+                            border: "1px solid #ef4444",
+                        },
+                    }}
+                />
             </AuthProvider>
         </BrowserRouter>
     </React.StrictMode>
