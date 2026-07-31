2c92ed405e7a3386ba39bad72bd4cadcedb60676
Deep <kumardeepranjan66@gmail.com>
2026-07-31 10:34:33 +0530

    Fix layout path with explicit jsx extension

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,6 +1,6 @@
 import { Routes, Route } from "react-router-dom";
 
-import Layout from "./components/layout/Layout";
+import Layout from "./components/layout/Layout.jsx";
 
 import Home from "./pages/Home";
 import Login from "./pages/Login";
