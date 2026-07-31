b547fa1a2672297709a46e651a4d024a0a867e93
Deep <kumardeepranjan66@gmail.com>
2026-07-31 09:54:24 +0530

    Set up automated GitHub Actions deploy

--- frontend/src/api/axios.js
+++ frontend/src/api/axios.js
@@ -1,7 +1,9 @@
 import axios from "axios";
 
 const api = axios.create({
-    baseURL: "http://localhost:8000/api/v1",
+    baseURL: process.env.NODE_ENV === "production" 
+        ? "https://onrender.com" 
+        : "http://localhost:8000/api/v1",
     withCredentials: true,
     headers: {
         "Content-Type": "application/json",
